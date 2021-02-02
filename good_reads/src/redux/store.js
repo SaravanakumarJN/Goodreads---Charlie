8155c946cc1d4056788df91dc85f4f895b28f09f
SaravanakumarJN <kumarjnsaravana@gmail.com>
2021-02-02 14:19:28 +0530

    initial store setup

--- good_reads/src/redux/store.js
+++ good_reads/src/redux/store.js
@@ -1,11 +1,11 @@
-import {applyMiddleware, combineReducers, createStore} from 'redux'
+import {applyMiddleware, combineReducers, compose, createStore} from 'redux'
 
 const rootReducer = combineReducers()
 
 const customThunks = store => next => action => {
     return typeof action === "function" ? action(store.dispatch) : next(action)
 }
-const composedEnhancer = (applyMiddleware(customThunks), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+const composedEnhancer = compose(applyMiddleware(customThunks), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 const store = createStore(rootReducer, composedEnhancer)
 
